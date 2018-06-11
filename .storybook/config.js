c06c89bebde81e802165c1386eee910b233a45f3
Mauricio Rodrigues <mmauricio.vsr@gmail.com>
2018-06-11 12:35:13 -0300

    configuring storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,12 @@
-import { configure } from '@storybook/react';
-import {setOptions} from '@storybook/addon-options';
+import { configure, addDecorator } from '@storybook/react';
+import { setOptions } from '@storybook/addon-options';
+import { withKnobs } from '@storybook/addon-knobs';
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /.stories.js$/);
+const req = require.context('../stories', true, /.story.js$/);
 function loadStories() {
     req.keys().forEach(filename => req(filename));
-    require('./playground.scss');
+    require('./stories.scss');
 }
 
 configure(loadStories, module);
