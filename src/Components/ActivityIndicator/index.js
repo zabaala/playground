f6186f5a87323d872b76d4c02890512fce60fae0
Mauricio Rodrigues <mmauricio.vsr@gmail.com>
2019-01-17 18:29:31 -0300

    fix: svg load

--- src/Components/ActivityIndicator/index.js
+++ src/Components/ActivityIndicator/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Rolling from '../../../images/svg/rolling.svg'
 
 const propTypes = {
     /**
@@ -24,7 +23,17 @@ const defaultProps = {
     height: 100
 };
 
-const ActivityIndicator = (props) =>  <Rolling {...props} />;
+const code = `data:image/svg+xml;base64,
+    PHN2ZyB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDAlIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxMDA
+    gMTAwIgogICAgIHByZXNlcnZlQXNwZWN0UmF0aW89InhNaWRZTWlkIiBjbGFzc05hbWU9Imxkcy1yb2xsaW5nIiA+CiAgICA8Y2lyY2xlIGN4PSI1
+    MCIgY3k9IjUwIiBmaWxsPSJub25lIiBzdHJva2U9IiMzZDg4ZWMiCiAgICAgICAgICAgIHN0cm9rZS13aWR0aD0iMTAiIHI9IjM1IiBzdHJva
+    2UtZGFzaGFycmF5PSIxNjQuOTMzNjE0MzEzNDY0MTUgNTYuOTc3ODcxNDM3ODIxMzgiCiAgICAgICAgICAgIHRyYW5zZm9ybT0icm90YXRlK
+    DguMzY5NDkgNTAgNTApIj4KICAgICAgICA8YW5pbWF0ZVRyYW5zZm9ybQogICAgICAgICAgICBhdHRyaWJ1dGVOYW1lPSJ0cmFuc2Zvcm0iC
+    iAgICAgICAgICAgIHR5cGU9InJvdGF0ZSIKICAgICAgICAgICAgY2FsY01vZGU9ImxpbmVhciIKICAgICAgICAgICAgdmFsdWVzPSIwI
+    DUwIDUwOzM2MCA1MCA1MCIKICAgICAgICAgICAga2V5VGltZXM9IjA7MSIKICAgICAgICAgICAgZHVyPSIwLjdzIgogICAgICAgICAgICBiZWdp
+    bj0iMHMiCiAgICAgICAgICAgIHJlcGVhdENvdW50PSJpbmRlZmluaXRlIiAvPgogICAgPC9jaXJjbGU+Cjwvc3ZnPg==`;
+
+const ActivityIndicator = (props) =>  <img src={code} {...props} />;
 
 ActivityIndicator.propTypes = propTypes;
 ActivityIndicator.defaultProps = defaultProps;
